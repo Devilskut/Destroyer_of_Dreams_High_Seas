f7117c9c1a3749ddbf4397434705696aa4815068
Devilskut <160415174+Devilskut@users.noreply.github.com>
2025-08-16 09:30:04 -0500

    DOD reboot
    Remove LFS, add player messages and add magazine updates

---
+++ Mods/0-SCore/Features/PassiveEffectHooks/SCoreMinEventTypes.cs
@@ -0,0 +1,10 @@
+public enum SCoreMinEventTypes
+{
+    onSelfQuestComplete = 200,
+    onQuestFailed = 201,
+    onSelfLockpickSuccess = 202,
+    onSelfScrapItem = 204,
+    onSelfCraftedRecipe = 205,
+    onSelfTurretDamagedOther = 206,
+    onSelfTurretKilledOther = 207
+}
